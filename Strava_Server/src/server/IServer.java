a92141cd3415ec73519654f462a7b5efe6638576
Andoni Torres <71408357+Trufatrula@users.noreply.github.com>
2022-11-10 13:20:52 +0100

    Server and IServer and ServerHandler does things, i dont know what, but it does do things
    Aimar my beloved

---
+++ Strava_Server/src/server/IServer.java
@@ -0,0 +1,14 @@
+package server;
+
+import java.util.List;
+
+public interface IServer {
+	//List<Session> activeSessions;
+	String registerByEmail(String email, String password);
+	String registerByGoogle(String email, String password);
+	String registerByFacebook(String email, String password);
+	String loginByEmail(String email, String password);
+	String loginByGoogle(String email, String password);
+	String loginByFacebook(String email, String password);
+	String logout(String token);
+}
--- Strava_Server/src/server/Server.java
+++ Strava_Server/src/server/Server.java
@@ -1,28 +1,175 @@
 package server;
 
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class Server {
+import es.deusto.ingenieria.sd.auctions.server.data.domain.User;
+import es.deusto.ingenieria.sd.auctions.server.services.LoginAppService;
+
+import java.lang.String;
+import java.rmi.RemoteException;
+
+public class Server implements IServer {
 	List<Session> activeSessions;
-	void registerByEmail() {
-		
+	private static final long serialVersionUID = 1L;
+
+	//Data structure for manage Server State
+	public Map<String, Session> serverState = new HashMap<>();
+	
+	 public String registerByEmail(String email, String password) {
+		 System.out.println(" * Email login: " + email + " / " + password);
+			//TODO DataBase hacer cuando
+			//Perform login() using LoginAppService
+	//TODO TODO TODO implementar cuando se haga el Session
+			//Session user = LoginAppService.getInstance().login(email, password);
+			Session user = new Session();
+			
+			//If login() success user is stored in the Server State
+			if (user != null) {
+				//If user is not logged in 
+				if (!this.serverState.values().contains(user)) {
+					String token = ""+ Calendar.getInstance().getTimeInMillis();		
+					this.serverState.put(token, user);		
+					return(token);
+				} else {
+					//TODO throw new RemoteException("User is already logged in!");
+					return("UnU");
+				}
+			} else {
+				//TODO throw new RemoteException("Login fails!");
+				return("uNu");
+			}
 	}
-	void registerByGoogle() {
+	public String registerByGoogle(String email, String password) {
+		System.out.println(" * Google Register: " + email + " / " + password);
+		//TODO cuando haya DB
+		//Perform login() using LoginAppService
+//TODO TODO TODO implementar cuando se haga el Session
+		//Session user = LoginAppService.getInstance().login(email, password);
+		Session user = new Session();
 		
+		//If login() success user is stored in the Server State
+		if (user != null) {
+			//If user is not logged in 
+			if (!this.serverState.values().contains(user)) {
+				String token = ""+ Calendar.getInstance().getTimeInMillis();		
+				this.serverState.put(token, user);		
+				return(token);
+			} else {
+				//TODO throw new RemoteException("User is already logged in!");
+				return("UnU");
+			}
+		} else {
+			//TODO throw new RemoteException("Login fails!");
+			return("uNu");
+		}
 	}
-	void registerByFacebook() {
+	public String registerByFacebook(String email, String password) {
+		System.out.println(" * FaceBook Register: " + email + " / " + password);
+		//TODO cuando haya base de datos
+		//Perform login() using LoginAppService
+//TODO TODO TODO implementar cuando se haga el Session
+		//Session user = LoginAppService.getInstance().login(email, password);
+		Session user = new Session();
 		
+		//If login() success user is stored in the Server State
+		if (user != null) {
+			//If user is not logged in 
+			if (!this.serverState.values().contains(user)) {
+				String token = ""+ Calendar.getInstance().getTimeInMillis();		
+				this.serverState.put(token, user);		
+				return(token);
+			} else {
+				//TODO throw new RemoteException("User is already logged in!");
+				return("UnU");
+			}
+		} else {
+			//TODO throw new RemoteException("Login fails!");
+			return("uNu");
+		}
 	}
-	void loginByEmail() {
-		
+	public String loginByEmail(String email, String password) {
+		System.out.println(" * Email login: " + email + " / " + password);
+				
+				//Perform login() using LoginAppService
+		//TODO TODO TODO implementar cuando se haga el Session
+				//Session user = LoginAppService.getInstance().login(email, password);
+				Session user = new Session();
+				
+				//If login() success user is stored in the Server State
+				if (user != null) {
+					//If user is not logged in 
+					if (!this.serverState.values().contains(user)) {
+						String token = ""+ Calendar.getInstance().getTimeInMillis();		
+						this.serverState.put(token, user);		
+						return(token);
+					} else {
+						//TODO throw new RemoteException("User is already logged in!");
+						return("UnU");
+					}
+				} else {
+					//TODO throw new RemoteException("Login fails!");
+					return("UnU");
+				}
 	}
-	void loginByGoogle() {
-		
+	public String loginByGoogle(String email, String password) {
+		System.out.println(" * Google login: " + email + " / " + password);
+				
+				//Perform login() using LoginAppService
+		//TODO TODO TODO implementar cuando se haga el Session
+				//Session user = LoginAppService.getInstance().login(email, password);
+				Session user = new Session();
+				
+				//If login() success user is stored in the Server State
+				if (user != null) {
+					//If user is not logged in 
+					if (!this.serverState.values().contains(user)) {
+						String token = ""+ Calendar.getInstance().getTimeInMillis();		
+						this.serverState.put(token, user);		
+						return(token);
+					} else {
+						//TODO throw new RemoteException("User is already logged in!");
+						return("UnU");
+					}
+				} else {
+					//TODO throw new RemoteException("Login fails!");
+					return("uNu");
+				}
 	}
-	void loginByFacebook() {
+	public String loginByFacebook(String email, String password) {
+		System.out.println(" * Facebook login: " + email + " / " + password);
 		
+		//Perform login() using LoginAppService
+//TODO TODO TODO implementar cuando se haga el Session
+		//Session user = LoginAppService.getInstance().login(email, password);
+		Session user = new Session();
+		
+		//If login() success user is stored in the Server State
+		if (user != null) {
+			//If user is not logged in 
+			if (!this.serverState.values().contains(user)) {
+				String token = ""+ Calendar.getInstance().getTimeInMillis();		
+				this.serverState.put(token, user);		
+				return(token);
+			} else {
+				//TODO throw new RemoteException("User is already logged in!");
+				return("UnU");
+			}
+		} else {
+			//TODO throw new RemoteException("Login fails!");
+			return("uNu");
+		}
 	}
-	void logout() {
+	public String logout(String token) {
+System.out.println(" * RemoteFacade logout: " + token);
 		
+		if (this.serverState.containsKey(token)) {
+			//Logout means remove the User from Server State
+			this.serverState.remove(token);
+		} else {
+			throw new RemoteException("User is not not logged in!");
+		}
 	}
 }
