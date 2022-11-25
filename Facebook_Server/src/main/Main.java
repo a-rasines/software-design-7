0916876824d20b8137c2d4fe7a2292571349b7c5
a-rasines <alberto.rasines@opendeusto.es>
2022-11-25 17:33:35 +0100

    (Maybe?) fixed Facebook

--- Facebook_Server/src/main/Main.java
+++ Facebook_Server/src/main/Main.java
@@ -13,15 +13,16 @@ public class Main {
 			System.out.println("created");
 			Socket s = ss.accept(); //Se queda esperando
 			System.out.println("accepted " + s);
-			DataInputStream dis = new DataInputStream(s.getInputStream());
-			String email = (String)dis.readUTF();
-			String password = (String) dis.readUTF();
-			
-			DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-			dos.writeUTF(Facebook_AppService.authenticate(email, password) +  "");
-			dos.flush();
-			dos.close();
-			ss.close();
+			while(true) {
+				DataInputStream dis = new DataInputStream(s.getInputStream());
+				String email = (String)dis.readUTF();
+				String password = (String) dis.readUTF();
+				
+				DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+				dos.writeUTF(Facebook_AppService.authenticate(email, password) +  "");
+				dos.flush();
+				dos.close();
+			}
 			
 		} catch(Exception e) {
 			System.out.println(":(");
--- Strava_Server/src/server/factory/FacebookAssembler.java
+++ Strava_Server/src/server/factory/FacebookAssembler.java
@@ -2,6 +2,7 @@ package server.factory;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 
 import server.data.temp.login.FacebookLoginDTO;
@@ -9,6 +10,15 @@ import server.data.temp.login.LoginDTO;
 
 public class FacebookAssembler implements AuthInterface {
 	private FacebookLoginDTO profile;
+	private static final Socket s = javaDoesntKnowHowToTreatATryStatement();
+	private static Socket javaDoesntKnowHowToTreatATryStatement() {
+		try {
+			return new Socket("localhost", 1101);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 	public FacebookAssembler(LoginDTO profile2) {
 		if(!(profile2 instanceof FacebookLoginDTO))
 			throw new IllegalArgumentException();
@@ -16,14 +26,13 @@ public class FacebookAssembler implements AuthInterface {
 	}
 	public boolean authenticate() {
 		try {
-			Socket s = new Socket("localhost", 1101);
 			DataOutputStream dout = new DataOutputStream(s.getOutputStream());
 			dout.writeUTF(profile.email + "\t" + profile.password);
 			dout.flush();
 			dout.close();
 			
 			DataInputStream din = new DataInputStream(s.getInputStream());
-			return true;
+			return Boolean.parseBoolean(din.readUTF());
 		} catch(Exception e) {
 			System.out.println("No good");
 			System.out.println(e);
