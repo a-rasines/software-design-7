644de78d490e434395e935f0714ed4d327c26879
a-rasines <alberto.rasines@opendeusto.es>
2022-11-29 20:18:36 +0100

    Me olvidé de los EmailProfile

--- Strava_Server/src/server/data/domain/DomainAssembler.java
+++ Strava_Server/src/server/data/domain/DomainAssembler.java
@@ -2,11 +2,34 @@ package server.data.domain;
 
 import java.util.ArrayList;
 
+import server.data.dto.ProfileTypeDTO;
 import server.data.dto.RegisterDTO;
 
 public class DomainAssembler {
 
 	public static Profile profileFromRegisterDTO(RegisterDTO dto) {
-		return new Profile(dto.getName(), dto.getBirthdate(), dto.getWeight(), dto.getHeight(), dto.getMaxHeartRate(), dto.getRestHeartRate(), dto.getEmail(), new ArrayList<>(), new ArrayList<>(), dto.getType());
+		if(dto.getType() == ProfileTypeDTO.EMAIL) {
+			return new EmailProfile(
+					dto.getName(), 
+					dto.getBirthdate(), 
+					dto.getWeight(), 
+					dto.getHeight(), 
+					dto.getMaxHeartRate(), 
+					dto.getRestHeartRate(), 
+					dto.getEmail(), 
+					new ArrayList<>(), 
+					new ArrayList<>());
+		}else
+			return new Profile(
+					dto.getName(), 
+					dto.getBirthdate(), 
+					dto.getWeight(), 
+					dto.getHeight(), 
+					dto.getMaxHeartRate(), 
+					dto.getRestHeartRate(), 
+					dto.getEmail(), 
+					new ArrayList<>(), 
+					new ArrayList<>(), 
+					dto.getType());
 	}
 }
