798ed0ac4e48e774cc5b24710df6576a35bdc4f0
a-rasines <alberto.rasines@opendeusto.es>
2022-12-02 11:38:50 +0100

    Update FacebookServer.java

--- Facebook_Server/src/main/FacebookServer.java
+++ Facebook_Server/src/main/FacebookServer.java
@@ -12,14 +12,13 @@ public class FacebookServer {
 			System.out.println("created");
 			Socket s = ss.accept(); //Se queda esperando
 			System.out.println("accepted " + s);
+			DataInputStream dis = new DataInputStream(s.getInputStream());
+			DataOutputStream dos = new DataOutputStream(s.getOutputStream());
 			while(true) {
-				DataInputStream dis = new DataInputStream(s.getInputStream());
 				String[] params = dis.readUTF().split("\t");
 				
-				DataOutputStream dos = new DataOutputStream(s.getOutputStream());
 				dos.writeUTF(Facebook_AppService.authenticate(params[0], params[1]) +  "");
 				dos.flush();
-				dos.close();
 			}
 			
 		} catch(Exception e) {
