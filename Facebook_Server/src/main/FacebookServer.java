0a9d3a6b7bbb42bb470bbacf1c352ac2086d9088
a-rasines <alberto.rasines@opendeusto.es>
2023-01-21 23:39:52 +0100

    El server de facebook no se cierra al perder conexion

--- Facebook_Server/src/main/FacebookServer.java
+++ Facebook_Server/src/main/FacebookServer.java
@@ -8,22 +8,24 @@ import java.net.Socket;
 public class FacebookServer {
 	public static void main(String[] args) {
 		//Create socket conection
-		try (ServerSocket ss = new ServerSocket(Integer.parseInt(args[0]))) {
-			System.out.println("created");
-			Socket s = ss.accept(); //Se queda esperando
-			System.out.println("accepted " + s);
-			DataInputStream dis = new DataInputStream(s.getInputStream());
-			DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-			while(true) {
-				String[] params = dis.readUTF().split("\t");
+		while(true) {
+			try (ServerSocket ss = new ServerSocket(Integer.parseInt(args[0]))) {
+				System.out.println("created");
+				Socket s = ss.accept(); //Se queda esperando
+				System.out.println("accepted " + s);
+				DataInputStream dis = new DataInputStream(s.getInputStream());
+				DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+				while(true) {
+					String[] params = dis.readUTF().split("\t");
 				
-				dos.writeUTF(Facebook_AppService.authenticate(params[0], params[1]) +  "");
-				dos.flush();
+					dos.writeUTF(Facebook_AppService.authenticate(params[0], params[1]) +  "");
+					dos.flush();
+				}
+				
+			} catch(Exception e) {
+				System.out.println(":(");
+				System.out.println(e);
 			}
-			
-		} catch(Exception e) {
-			System.out.println(":(");
-			System.out.println(e);
 		}
 	}
 }
